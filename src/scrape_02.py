a827bf0cc8d37ddbd0f383da5551f73c7befacbf
montaquila <akiel@akiel.org>
2022-10-15 19:13:08 -0700

    cleaned up Product class and methods. Added jira formatting file for our product to print neatly to console

--- src/scrape_02.py
+++ src/scrape_02.py
@@ -1,19 +1,24 @@
 """
-Simple scraping with beautifulsoup + requests. Spoofing machine + browser information
-is necessary in order to extract data from amazon and perhaps other major
-companies.
+Simple scraping with beautifulsoup + requests. Spoofing machine + browser 
+information is necessary in order to extract data from amazon and perhaps 
+other major companies.
 
 This file's purpose is to extract data from a given URL
 """
+
 # lib for scraping + formatting retrieved data
 from bs4 import BeautifulSoup as bs
 # python binding for C libs libxml libxslt, handling html xml files
 import lxml
 # lib for HTTP requests
 import requests
+#  
+from jinja2 import Environment as j2_ENV
+# 
+from jinja2 import FileSystemLoader as j2_FSL
 
 
-class product:
+class Product:
     """
     product class that will provide easier use of inheritance for other classes
     relying off the data collected from this class. 
@@ -22,17 +27,44 @@ class product:
         - details()
         - desc()
         - price()
-        - ratings()
+        - rating()
         - num_ratings()
     """
 
-    """
-    create a constructor for this class. could save every field value collected
-    to a dataframe, list, dict, etc...
+    def __init__(self, soup):
+        """
+        Product class constructor, each 'field' function of the class 
+        is assigned to an attribute of the class
+        """
+        self.soup = soup
+        self.name = self.get_name()
+        self.details = self.get_details()
+        self.desc = self.get_desc()
+        self.price = self.get_price()
+        self.rating = self.get_rating()
+        self.num_ratings = self.get_num_ratings()
+        self.display = self.print_contents()
+    
+    def print_contents(self):
+        """
+        takes care of formatting neatly with Jira + printing
+        """
+        # load the folder in which the template is located
+        env = j2_ENV(loader = j2_FSL(''))
+        # load the .j2 template
+        template = env.get_template('product.j2')
+        # define our fields to be rendered based on the .j2 file
+        content_fmt = template.render(name=self.name, 
+                                      details=self.details, 
+                                      desc=self.desc, 
+                                      price=self.price, 
+                                      rating=self.rating, 
+                                      num_ratings=self.num_ratings)
+        # print our formatted content
+        print(content_fmt)
+        # print("Product: %s\nDetails: %s\nDescription: %s" % (self.name, self.details, self.desc))
 
-    def __init__(self):
-    """
-    def name(soup):
+    def get_name(self):
         """
         Finds the name of the product by parsing the webpage. Inspect the 
         desired webpage and the value to save
@@ -41,23 +73,19 @@ class product:
             - 'productTitle' = the HTML we want to save
         """
         try:
-            # declare the outer span inline container tag as starting parse point
-            product = soup.find('span', attrs = {'id': 'productTitle'})
-
+            # outer span inline container tag as starting parse point
+            product = self.soup.find('span', attrs = {'id': 'productTitle'})
             # convert product to a string
-            product_name = product.string
-            
+            name = product.string
             #formatting, neglecting commas and whitespace
-            product_fmt = product_name.strip().replace(',', '')
+            name_fmt = name.strip().replace(',', '')
 
         except AttributeError:
-            product_fmt = "PRODUCT NAME NOT FOUND"
+            name_fmt = "PRODUCT NAME NOT FOUND"
 
-        # print the product name
-        print("Product: \n", product_fmt)
+        return name_fmt
 
-        
-    def details(soup):
+    def get_details(self):
         """
         function to retrieve details of an amazon product
             - 'div' = the starting division to begin parsing
@@ -65,19 +93,16 @@ class product:
             - 'productOverview_feature_div' = the HTML we want to save
         """
         try:
-            details = soup.find('div', attrs = {'id': 'productOverview_feature_div'})
-
+            details = self.soup.find('div', attrs = {'id': 'productOverview_feature_div'})
             product_details = details.string
-
             details_fmt = product_details.strip().replace(',', '')   
 
         except AttributeError:
             details_fmt = "PRODUCT DETAILS NOT FOUND"
             
-        print("Product Details: \n", details_fmt)
+        return details_fmt
 
-
-    def desc(soup):
+    def get_desc(self):
         """
         function to retrieve the description from of amazon product
             - 'li' = the starting list to begin parsing
@@ -85,7 +110,7 @@ class product:
             - 'a-spacing-small' = the HTML we want to save
         """
         try:
-            desc = soup.find('li', attrs = {'id': 'a-spacing-small'})
+            desc = self.soup.find('li', attrs = {'id': 'a-spacing-small'})
 
             product_desc = desc.string
 
@@ -94,10 +119,10 @@ class product:
         except AttributeError:
             desc_fmt = "PRODUCT DESC NOT FOUND"
 
-        print("Product Description: \n", desc_fmt)
+        #print("Product Description: \n", desc_fmt)
+        return desc_fmt
 
-
-    def price(soup):
+    def get_price(self):
         """
         function to retrieve the price info of an amazon product
             - 'span' = the starting inline container
@@ -105,16 +130,16 @@ class product:
             - 'a-offscreen' = the HTML we want to save
         """
         try:
-            price = soup.find('span', attrs = 
+            price = self.soup.find('span', attrs = 
                               {'class': 'a-offscreen'}).string.strip().replace(',', '')
         
         except AttributeError:
             price = "PRICE NOT FOUND"
         
-        print("Price (USD): \n", price)
-
+        #print("Price (USD): \n", price)
+        return price
         
-    def ratings(soup):
+    def get_rating(self):
         """
         function to retrieve the rating of an amazon product
             - 'span' = the starting inline container
@@ -122,15 +147,15 @@ class product:
             - 'a-icon-alt' = element to save
         """
         try:
-            rating = soup.find("span", attrs =
+            rating = self.soup.find("span", attrs =
                                {'class': 'a-icon-alt'}).string.strip().replace(',', '')
         except AttributeError:
                 rating = "RATING NOT FOUND"
         
-        print("Rating: \n", rating)
+        #print("Rating: \n", rating)
+        return rating
 
-
-    def num_ratings(soup):
+    def get_num_ratings(self):
         """
         function to retrieve the number of ratings an amazon product has been
         given
@@ -139,15 +164,14 @@ class product:
             - 'acrCustomerReviewText' = the HTML we want to save
         """
         try:
-            ratings_num = soup.find('span', attrs = 
+            ratings_num = self.soup.find('span', attrs = 
                         {'id': 'acrCustomerReviewText'}).string.strip().replace(',', '')
 
         except AttributeError:
             ratings_num = "NUMBER OF RATINGS NOT FOUND"
         
-        print("Number of Ratings: \n", ratings_num)
-
-
+        #print("Number of Ratings: \n", ratings_num)
+        return ratings_num
 
 def main():
     """
@@ -155,12 +179,6 @@ def main():
     for something stable or server-based, think of a function to parse a list
     of user agents and swap on a time-based interval
     """
-    #user_agent = ({'User-Agent': 
-    #               'Mozilla/5.0 (X11; Linux x86_64) \
-    #               AppleWebKit/537.36 (KHTML, like Gecko) \
-    #               Chrome/44.0.2403.157 Safari/537.36', \
-    #               'Accept-Language': 'en-US, en;q=0.5'
-    #               })
     user_agent = ({'User-Agent': 
                     'Mozilla/5.0 (X11; Linux x86_64) \
                     AppleWebKit/537.36 (KHTML, like Gecko) \
@@ -170,30 +188,28 @@ def main():
 
     # source file
     link = 'https://www.amazon.com/AMD-Ryzen-5600X-12-Thread-Processor/dp/B08166SLDF/ref=sr_1_1?crid=1B7QEPTT5XCBH&keywords=amd+ryzen+5000&qid=1665786908&qu=eyJxc2MiOiIzLjA1IiwicXNhIjoiMi44NCIsInFzcCI6IjIuMTUifQ%3D%3D&sprefix=amd%2520ryzen%25205000%2Caps%2C114&sr=8-1&ufe=app_do%3Aamzn1.fos.f5122f16-c3e8-4386-bf32-63e904010ad0'
-
+    
     # HTTP request 
     page = requests.get(link, headers = user_agent)
 
     # soup object that stores our data
     soup = bs(page.content, "lxml")
-
+    
     """
-    Call our functions that will return the required fields:
-        - Product Name
-        - Product Details
-        - Product Description
-        - Product Price
-        - Product Ratings
-        - Product # of Ratings
+    call to our class which calls our constructor simultaneously calls the 
+    functions to fill in our product, passing in the soup object. within the 
+    construct we call the following functions:
+        - get_name()
+        - get_details() 
+        - get_desc()
+        - get_price()
+        - get_rating()
+        - get_num_ratings()
+        - print_contents() 
     """
-    product.name(soup)
-    product.details(soup)
-    product.desc(soup)
-    product.price(soup)
-    product.ratings(soup)
-    product.num_ratings(soup)
-
+    p = Product(soup)
 
 if __name__ == '__main__':
     main()
+
 
